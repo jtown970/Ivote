d10b425ccdd285685c6a657186b148d26c8a5da4
jtown970 <jasonrtowner@gmail.com>
2020-06-16 21:52:03 -0700

    add ; to sql seed file

--- db/seed.sql
+++ db/seed.sql
@@ -34,7 +34,16 @@ create table house_votes (
 ); 
 
 select user_votes_id, user_id, item, user_name, location, vote_yes from users
-join user_votes on users.user_id = user_votes.user_votes_id
+join user_votes on users.user_id = user_votes.user_votes_id;
 
 select house_id, rep_name, location, house_votes_id, reps_id, item, passed, voted_yes from house
-join house_votes on house.house_id = house_votes.house_votes_id
+join house_votes on house.house_id = house_votes.house_votes_id;
+
+
+
+
+
+
+
+
+
